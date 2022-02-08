ee6349c0e79a11235b77c7f29307ab1413bb6725
pratham792 <98786259+pratham792@users.noreply.github.com>
2022-02-07 16:12:12 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("hello this is pratham");
+const animal paraghraphs = {
+    "mission": "Best view of SF skyline",
+    "golden gate": "Biggest park in SF"
+    "Nob Hill": "closest to my home"
+    "Botanical Gardens": "Expensice af"
+    "Yerba Buena": "idk"
+}
